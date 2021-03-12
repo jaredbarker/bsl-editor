d7e818a8d721305045431ef112681c76ed07c770
Jared Barker <jaredbarker99@gmail.com>
2021-03-11 22:27:44 -0700

    Added note grid
    Added a canvas element to the left pane, and drew some squares in the general pattern we want

--- src/gui_objects/left/LeftPane.java
+++ src/gui_objects/left/LeftPane.java
@@ -3,14 +3,22 @@ package gui_objects.left;
 import Controls.ProgramState;
 import Controls.ProgramStateListener;
 import gui_objects.right.RightButtonsEnum;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 
 public class LeftPane extends BorderPane implements ProgramStateListener {
 
-    ProgramState state;
-
+    private ProgramState state;
+    private ScrollPane mainScrollPane;
+    private Canvas canvas;
+    private GraphicsContext noteArea;
+    private int canvasWidth = 900;
+    private int canvasHeight = 600;
 
     public LeftPane(ProgramState state) {
         super();
@@ -18,13 +26,39 @@ public class LeftPane extends BorderPane implements ProgramStateListener {
         this.state.addListener(this);
         this.setMinWidth(500);
         this.setMinHeight(500);
-        this.setTop(new TextField("Events     Base Notes      Baritone Notes        Tenor Notes     Obstacles"));
+        this.setMaxWidth(canvasWidth);
+        this.setMaxHeight(canvasHeight);
+        this.setTop(new TextField("Audio Strip            Base Notes      Baritone Notes        Tenor Notes     Obstacles     Events"));
 
-        this.setCenter(new TextField("Center Left"));
+
+        this.canvas = new Canvas(canvasWidth, canvasHeight);
+        this.mainScrollPane = new ScrollPane();
+        this.noteArea = canvas.getGraphicsContext2D();
+        drawNotes(noteArea);
+        this.mainScrollPane.setContent(this.canvas);
+
+        this.setCenter(this.mainScrollPane);
     }
 
     @Override
     public void rightToolBarSelection(RightButtonsEnum buttonPress) {
 
     }
+
+    private void drawNotes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.GREEN);
+        gc.setLineWidth(2);
+        int size = 15;
+        int audioStripOffset = size * 14;
+
+        for (int row = 0; row < canvasWidth; row += size) {
+            for (int col = audioStripOffset; col < canvasWidth; col += size) {
+                if (((col - audioStripOffset) % (6 * size) != size * 4) && ((col - audioStripOffset) % (6 * size) != size * 5)) {
+                    gc.strokeRoundRect(col, row, size, size, 2, 2);
+                }
+            }
+        }
+    }
+
 }
