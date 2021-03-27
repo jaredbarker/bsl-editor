3a92e53c6c5d7cda3d4fb7273b8a9e7ec3f9461d
Brian Barker <brianbarker92@gmail.com>
2021-03-27 16:10:20 -0700

    Added Demo Media Players
    Need these modules in your run configuration to run them:
    --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.media

---
+++ src/sample/JaredTestMedia/MediaExample.java
@@ -0,0 +1,35 @@
+package sample.JaredTestMedia;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+public class MediaExample extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws MalformedURLException {
+        File mediaFile = new File("yourfilepathhere");
+        Media media = new Media(mediaFile.toURI().toURL().toString());
+
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+        MediaView mediaView = new MediaView(mediaPlayer);
+
+        Scene scene = new Scene(new Pane(mediaView), 1024, 800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        mediaPlayer.play();
+
+    }
+}
